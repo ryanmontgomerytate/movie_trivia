8c1eee93bbd87657961b8b9b38daee01d50058f6
Ryan Tate <ryantate@Joshuas-MBP.attlocal.net>
2022-01-02 19:51:45 -0600

    crafted quiz questions and display them

--- src/Quiz.tsx
+++ src/Quiz.tsx
@@ -1,19 +1,41 @@
-import { MovieAnswers, MovieQuestions } from './MovieTypes'
+import { useState } from 'react'
+import { MovieAnswers, MovieQuestion, MovieQuestions } from './MovieTypes'
+
+export const generateAnswerButtons = (movieAnswers: MovieAnswers[]) => {
+  return movieAnswers.map((movieAnswer) => (
+    <div>
+      <button>{movieAnswer.Title}</button>
+      <br></br>
+    </div>
+  ))
+}
+
+export const getRandomQ = (max: number) => {
+  return Math.floor(Math.random() * max)
+}
 
 export const Quiz: React.FC<{ movieAnswers: MovieAnswers[] }> = ({
   movieAnswers: movieAnswers,
 }) => {
+  const [index, setIndex] = useState<number>(getRandomQ(movieAnswers.length))
+  const [quizQuestionNumber, setQuizQuestionNumber] = useState<number>(0)
+  const handleNext = () => {
+    setIndex(getRandomQ(movieAnswers.length))
+    setQuizQuestionNumber(quizQuestionNumber + 1)
+  }
+  const currentQuestion: MovieQuestion = MovieQuestions[quizQuestionNumber]
+
   return (
     <div>
       <div>
-        <h2>{movieAnswers[0].Title}</h2>
-        <p>{MovieQuestions.ACTOR_QUESTION}</p>
-        <p>{movieAnswers[0].Actors}</p>
-      </div>
-      <div>
-        <h2>{movieAnswers[0].Title}</h2>
-        <p>{MovieQuestions.MOVIE_RATINGS}</p>
-        <p>{movieAnswers[0].Ratings[0].Value}</p>
+        <h2>{`${currentQuestion.question} ${
+          movieAnswers[index][currentQuestion.answerPropertyName]
+        }`}</h2>
+        {generateAnswerButtons(movieAnswers)}
+        <br />
+        {quizQuestionNumber < MovieQuestions.length && (
+          <button onClick={handleNext}>Next</button>
+        )}
       </div>
     </div>
   )
