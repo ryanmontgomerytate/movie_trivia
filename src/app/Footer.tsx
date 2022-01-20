871ffa67fbc5a4bde4764522dbb6e6e88bf71c13
Ryan Tate <ryantate@Joshuas-MBP.attlocal.net>
2022-01-20 17:00:16 -0600

    cosmetic changes

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,21 +1,34 @@
-import styled from '@emotion/styled'
-import { Tooltip, Typography } from '@mui/material'
+
+import { Tooltip, Typography, Link } from '@mui/material'
 import React from 'react'
 
 export const Footer: React.FC = () => {
   return (
     <div className="footer">
-      <Typography noWrap component="div" sx={{ color: 'white' }}>
-        Built from
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        noWrap
+        component="div"
+        sx={{ color: 'white' }}
+      >
         <Tooltip title="http://www.omdbapi.com/">
-          <FooterLink href="http://www.omdbapi.com/"> OMDb API</FooterLink>
+          <Link href="http://www.omdbapi.com/" underline="none" color="white">
+            {' '}
+            Quiz generated using data from OMDb API.&nbsp;
+          </Link>
+        </Tooltip>
+
+        <Tooltip title="https://www.flaticon.com/free-icons/popcorn">
+          <Link
+            href="https://www.flaticon.com/free-icons/popcorn"
+            underline="none"
+            color="white"
+          >
+             &nbsp;Popcorn favicon created by Adib Sulthon, Flaticon.
+          </Link>
         </Tooltip>
       </Typography>
     </div>
   )
 }
-
-const FooterLink = styled.a`
-  text-decoration: none;
-  color: white;
-`
